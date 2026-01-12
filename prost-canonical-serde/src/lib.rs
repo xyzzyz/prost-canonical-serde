296df7ff3de1e59f76c7400ed2a57a102dffc7a1
Adam Michalik <adam@michalik.es>
2026-01-11 20:34:19 -0800

    Fix the example so that the main crate can be published (#4)

--- prost-canonical-serde/src/lib.rs
+++ prost-canonical-serde/src/lib.rs
@@ -14,18 +14,17 @@
 //!
 //! ## example.proto
 //! ```proto
-#![doc = include_str!("../../prost-canonical-serde-example/proto/example.proto")]
+#![doc = include_str!("../docs/proto/example.proto")]
 //! ```
 //!
 //! ## build.rs
 //! ```rust,ignore
-#![doc = include_str!("../../prost-canonical-serde-example/build.rs")]
+#![doc = include_str!("../docs/example_build.rs")]
 //! ```
 //!
 //! ## usage
 //! ```rust
-#![doc = include_str!("../../prost-canonical-serde-example/docs/usage.rs")]
-//! # main();
+#![doc = include_str!("../docs/usage.rs")]
 //! ```
 //!
 //! The derive macros generate canonical protobuf JSON serde implementations, so
